25d29e322b9feea2b7eeae48c74d823eae97ee63
Nadia Layra Aziza <nadialayra15@gmail.com>
2022-06-15 21:05:04 +0700

    Config rirebase baru

--- UAS_PBF_BACKEND/src/config.js
+++ UAS_PBF_BACKEND/src/config.js
@@ -1,21 +1,13 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  // apiKey: "AIzaSyCznrQai42JZ1-kCLnQ-BmBltDMXvTfKS4",
-  // authDomain: "fir-login-nadia.firebaseapp.com",
-  // databaseURL: "https://fir-login-nadia-default-rtdb.firebaseio.com",
-  // projectId: "fir-login-nadia",
-  // storageBucket: "fir-login-nadia.appspot.com",
-  // messagingSenderId: "498100703771",
-  // appId: "1:498100703771:web:9d54505a167aba93c55ebe",
-  // measurementId: "G-3716JKHZV5"
-  apiKey: "AIzaSyCOFPzHWlCn1xQRLZM8WxC_wkn1zO7S2nA",
-  authDomain: "react-firebase-64e9e.firebaseapp.com",
-  databaseURL: "https://react-firebase-64e9e-default-rtdb.firebaseio.com",
-  projectId: "react-firebase-64e9e",
-  storageBucket: "react-firebase-64e9e.appspot.com",
-  messagingSenderId: "657983664620",
-  appId: "1:657983664620:web:e9f9ea96f52339d498d0c9",
-  measurementId: "G-X1RJ5G0MYW"
+  apiKey: "AIzaSyClKMSKbgCcf58jfg045tb2cv29ehl36fQ",
+  authDomain: "fir-login-shelyca.firebaseapp.com",
+  databaseURL: "https://fir-login-shelyca-default-rtdb.firebaseio.com",
+  projectId: "fir-login-shelyca",
+  storageBucket: "fir-login-shelyca.appspot.com",
+  messagingSenderId: "966934968015",
+  appId: "1:966934968015:web:b7b98675ecdf6f31cba9ba",
+  measurementId: "G-HQPMKNGLMR"
 };
 
 export default firebaseConfig;
